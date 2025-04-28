585ed49f426c4372ee00d2be9b8f7b50f025fa8d
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2025-04-28 12:47:40 +0200

    init
    weil thomas auf flasche sitzt

---
+++ src/main/java/htlkaindorf/clashtoolsbackend/ClashtoolsBackendApplication.java
@@ -0,0 +1,13 @@
+package htlkaindorf.clashtoolsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashtoolsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClashtoolsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/htlkaindorf/clashtoolsbackend/ClashtoolsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package htlkaindorf.clashtoolsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClashtoolsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
