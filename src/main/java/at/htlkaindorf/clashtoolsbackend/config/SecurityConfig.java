9d723fcc65f6928dc4b066bdb52a9d70238b0cfb
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2025-06-09 00:13:31 +0200

    add Patch to CORS

--- src/main/java/at/htlkaindorf/clashtoolsbackend/config/SecurityConfig.java
+++ src/main/java/at/htlkaindorf/clashtoolsbackend/config/SecurityConfig.java
@@ -97,7 +97,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of("http://localhost:5173", "http://localhost:8080"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
